01989d9e8ed40ec363860128b37ed007c5d594a3
Faris <faris.223040126@mail.unpas.ac.id>
2024-10-29 19:46:21 +0700

    Laravel 11 Video 5 - Blade Component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
-    <a href="/">home</a>
-    <a href="/about">about</a>
-    <a href="/blog">blog</a>
-    <a href="/contact">contact</a>
-    <h1>Halaman Contact</h1>
-
-    <ul>
-        <li>
-            <a href="https://www.instagram.com">Instagram</a>
-        </li>
-        <li>
-            <a href="https://www.tiktok.com">Tiktok</a>
-        </li>
-        <li>
-            <a href="https://github.com">GitHub</a>
-        </li>
-    </ul>
-
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman Contact</h3>
+</x-layout>
