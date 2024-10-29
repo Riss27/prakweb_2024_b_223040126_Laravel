86ae4fe19c9e2d78359ab1597a754fd6e200ea3c
Faris <faris.223040126@mail.unpas.ac.id>
2024-10-29 22:49:29 +0700

    Laravel 11 Video 7 - Model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -12,45 +16,12 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title'=> 'Blog', 'posts' => [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Faris',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sit ipsam eaque totam modi? Expedita consequuntur beatae doloremque asperiores vero recusandae incidunt maiores voluptatem, quasi alias necessitatibus ipsum deleniti pariatur!'
-        ],
-        [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Faris',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore recusandae, accusantium fugiat consectetur quae corrupti, debitis hic nobis vel delectus veniam facilis. Vitae dolore tempore excepturi rerum, nam repudiandae error!'
-        ]
-    ]]);
+    return view('posts', ['title'=> 'Blog', 'posts' => Post::all()]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-    [
-        'id' => '1',
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel 1',
-        'author' => 'Faris',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sit ipsam eaque totam modi? Expedita consequuntur beatae doloremque asperiores vero recusandae incidunt maiores voluptatem, quasi alias necessitatibus ipsum deleniti pariatur!'
-    ],
-    [
-        'id' => '2',
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2',
-        'author' => 'Faris',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore recusandae, accusantium fugiat consectetur quae corrupti, debitis hic nobis vel delectus veniam facilis. Vitae dolore tempore excepturi rerum, nam repudiandae error!'
-    ]
-    ];
 
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find( $slug );
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
